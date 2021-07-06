6dd9348865ad7300c8c4aa55da636fc960678de8
Anthony Catullo <acatullo4@gmail.com>
2021-07-05 22:55:39 -0500

    add jsx for left portion of app

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import IngredientList from './IngredientList';
 
 export default function Recipe(props) {
-    
+
     const {
         name,
         cookTime,
         servings,
-        instructions
+        instructions,
+        ingredients
     } = props
-    
+
     return (
         <div>
             <div>
@@ -37,6 +39,12 @@ export default function Recipe(props) {
             </div>
 
 
+            <div>
+                <span>Ingredients: </span>
+                <div>
+                    <IngredientList ingredients={ingredients} />
+                </div>
+            </div>
         </div>
     )
 }
